c92cc018b27e8f916b9bcd6246719001e1ccdccc
rzlstprnma <grtkqs@gmail.com>
2019-04-21 01:15:45 +0700

    Landing Page only desktop

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+new Glider(document.querySelector('.glider'),{
+    slidesToShow: 2.5,
+    
+    draggable: true,
+
+});
+
+let container = document.querySelector('.container-portfolio')
+let jumbo = document.querySelector('.jumbo')
+let thumbs = document.querySelectorAll('.thumb')
+container.addEventListener('click', function(e){
+
+    if(e.target.className == 'thumb'){
+
+        jumbo.src = e.target.src
+        jumbo.classList.add('fade')
+        setTimeout(function(){
+            jumbo.classList.remove('fade')
+        }, 500);
+
+        thumbs.forEach(function(thumb){
+            if( thumb.classList.contains('active') ){
+                thumb.classList.remove('active')
+            }
+            // thumb.className = 'thumb'
+        });
+         
+
+        e.target.classList.add('active')
+
+    }
+
+})
